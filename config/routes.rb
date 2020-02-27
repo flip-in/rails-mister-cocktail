55a5ee8abeb2539338a2f4c2f2b4cb05c5e2c5ee
Will Price <pricew86@gmail.com>
2020-02-27 16:25:39 +0800

    starting to style

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails do
-    resources :doses, only: [:new]
-  end
+  # resources :cocktails do
+  #   resources :doses, only: [:new]
+  # end
+  #COCKTAILS
+  get '/cocktails', to: 'cocktails#index', as: :cocktails
+  get '/cocktails/new', to: 'cocktails#new', as: :new_cocktail
+  get '/cocktails/:id', to: 'cocktails#show', as: :cocktail
+  post '/cocktails', to: 'cocktails#create'
+  #DOSES
+  get '/cocktails/:cocktail_id/doses/new', to: 'doses#new', as: :new_dose
+  post '/cocktails/:cocktail_id/doses', to: 'doses#create'
+  delete '/doses/:id', to: 'doses#destroy'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Seeding the db with a few drinks..."
 
-Ingredient.create(name: “vodka”)
-Ingredient.create(name: “tomato juice”)
-Ingredient.create(name: “mint leaves”)
-Cocktail.create(name: “Mohito”)
-Cocktail.create(name: “Bloody Marry”)
-Cocktail.create(name: “Margarita”)
-Dose.create(description: “6 leaves”, cocktail_id: 1, ingredient_id: 3)
-Dose.create(description: “90ml”, cocktail_id: 2, ingredient_id: 2)
-Dose.create(description: “30ml”, cocktail_id: 2, ingredient_id: 1)
+Ingredient.create(name: 'vodka')
+Ingredient.create(name: 'tomato juice')
+Ingredient.create(name: 'mint leaves')
+Cocktail.create(name: 'Mohito')
+Cocktail.create(name: 'Bloody Marry')
+Cocktail.create(name: 'Margarita')
+Dose.create(description: '6 leaves', cocktail_id: 1, ingredient_id: 3)
+Dose.create(description: '90ml', cocktail_id: 2, ingredient_id: 2)
+Dose.create(description: '30ml', cocktail_id: 2, ingredient_id: 1)
 
 puts "...done!"
