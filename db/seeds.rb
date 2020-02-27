12d5066d8099f918c73f24e7f56fd9f265293a6e
Will Price <pricew86@gmail.com>
2020-02-27 13:13:36 +0800

    init models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding the db with a few drinks..."
+
+Ingredient.create(name: “vodka”)
+Ingredient.create(name: “tomato juice”)
+Ingredient.create(name: “mint leaves”)
+Cocktail.create(name: “Mohito”)
+Cocktail.create(name: “Bloody Marry”)
+Cocktail.create(name: “Margarita”)
+Dose.create(description: “6 leaves”, cocktail_id: 1, ingredient_id: 3)
+Dose.create(description: “90ml”, cocktail_id: 2, ingredient_id: 2)
+Dose.create(description: “30ml”, cocktail_id: 2, ingredient_id: 1)
+
+puts "...done!"
