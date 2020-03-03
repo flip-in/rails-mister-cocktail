919b9c8141b9567a0be46736eec9554d5ec28724
Will Price <pricew86@gmail.com>
2020-03-03 09:09:46 +0800

    squashed bugs

--- app/javascript/_form.js
+++ app/javascript/_form.js
@@ -2,12 +2,15 @@ const show_form = () => {
   const formButton = document.querySelector('#formButton')
   const form = document.querySelector('.form-container')
   const cancel = document.querySelector('#cancelButton')
-  formButton.addEventListener('click', function () {
-    form.classList.toggle('d-none');
-  });
-  cancel.addEventListener('click', function () {
-    form.classList.toggle('d-done');
-  });
+  if (form) {
+    formButton.addEventListener('click', function () {
+      form.classList.toggle('d-none');
+    });
+    cancel.addEventListener('click', function () {
+      form.classList.toggle('d-done');
+    });
+
+  }
 }
 
 export {show_form}
